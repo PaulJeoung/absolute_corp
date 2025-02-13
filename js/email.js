d14c12941a97ac3cba542fb533944d7e4bc75f2c
Paul v2 <114468411+PaulJeoung@users.noreply.github.com>
2025-02-13 17:08:05 +0900

    console log added

--- js/email.js
+++ js/email.js
@@ -1,22 +1,23 @@
-// Email.js 라이브러리 초기화
+// Email.js initalize
 (function() {
     emailjs.init("5ndp95bdoqP1J2fLz"); // publicKey from Email.js
 })();
 
-// 문서가 로드되면 이벤트 리스너 추가
+// Event listener added when loaded in DOMcontent
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".contact-form");
 
     if (form) {
         form.addEventListener("submit", function (event) {
-            event.preventDefault(); // 기본 폼 제출 방지
+            event.preventDefault(); // prevent default form
+            console.log("submit click envent excution");
 
-            // 입력된 값 가져오기
+            // getter in value
             const name = document.getElementById("name").value;
             const email = document.getElementById("email").value;
             const message = document.getElementById("message").value;
 
-            // 필수 입력값 확인
+            // input data validation
             if (!name) {
                 alert("이름을 입력해 주세요.");
                 document.getElementById("name").focus();
@@ -33,26 +34,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 return;
             }
 
-            // 체크박스 값 가져오기
+            // getter in checkbox value
             let services = [];
             document.querySelectorAll(".form-check-input:checked").forEach((checkbox) => {
                 services.push(checkbox.nextElementSibling.innerText);
             });
 
-            // 이메일 전송 데이터
+            // set on email form data
             const templateParams = {
                 to_name : "Absolute Solution 님",
                 from_name: name,
                 from_email: email,
                 message: message,
-                services: services.join(", "), // 선택된 서비스 목록
+                services: services.join(", "), // selected service list
             };
 
-            // Email.js를 사용하여 메일 전송
+            // Email.js sent logic
+            console.log("preparing mailing service set on, email.js smtp send now\n", templateParams)
             emailjs.send("service_nwpfdn5", "template_6hewwfg", templateParams) // "service_id", "template_id", templateParams
                 .then(function (response) {
                     alert("메일이 성공적으로 발송되었습니다!");
-                    form.reset(); // 폼 초기화
+                    form.reset(); // form initialize
                 }, function (error) {
                     alert("메일 발송에 실패했습니다. 다시 시도해 주세요.");
                     console.error("EmailJS Error:", error);
